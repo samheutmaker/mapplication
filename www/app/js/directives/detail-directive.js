75ea20f8a70f34f3a293bd2c15875d51d7e9ef1b
SamHeutmaker <samheutmaker@gmail.com>
2016-03-01 18:25:22 -0800

    MAP PINS NOT WORKING

--- www/app/js/directives/detail-directive.js
+++ www/app/js/directives/detail-directive.js
@@ -1,23 +1,47 @@
 module.exports = function(app) {
-	app.directive('pinDetail', function() {
-		return {
-			restrict: 'AEC',
-			replace: true,
-			templateUrl: 'templates/detail.html',
-			scope: {
-				activePin: '=',
-				show: '='
-			},
-			link: function(scope, elems, attr){
-				scope.$watch(function() {
-					return scope.activePin;
-				}, function() {
-					console.log(scope.activePin);
-				});
-			},
-			controller: function($scope) {
-				console.log($scope.show);
-			}
-		}
-	});
+  app.directive('pinDetail', function() {
+    return {
+      restrict: 'AEC',
+      replace: true,
+      templateUrl: 'templates/detail.html',
+      scope: {
+        activePin: '=',
+        show: '='
+      },
+      link: function($scope, elems, attr) {
+        $scope.$watch(function() {
+          return $scope.activePin
+        }, function() {
+        	$scope.getComments($scope.activePin._id);
+        });
+      },
+      controller: function($scope, Comment) {
+        // Post new comment
+        $scope.postComment = function(newComment, pinId) {
+          if (newComment.content.length > 7) {
+            Comment.postComment(newComment, pinId).then(function(res) {
+              if ($scope.activePin.comments)
+                $scope.activePin.comments.push(res.data);
+            });
+          }
+        }
+        // Get all comments for a post
+        $scope.getComments = function(pinId) {
+          $scope.activePin.comments = [];
+          Comment.getComments(pinId).then(function(res) {
+            $scope.activePin.comments = res.data
+          });
+        };
+
+        // Remove comment from DB
+        $scope.removeComment = function(comment) {
+          $scope.
+          Comment.removeComment(comment._id).then(function(res) {
+
+          })
+        }
+
+      }
+    }
+  });
 }
--- www/build/bundle.js
+++ www/build/bundle.js
@@ -156,8 +156,6 @@
 	    $scope.searchPins = [];
 	    // Currently active pin
 	    $scope.activePin = {};
-	    // Active Pin Comments
-	    $scope.comments = {};
 	    // Empty Map Object
 	    $scope.map = null;
 	    // Hide Controls
@@ -221,16 +219,14 @@
 
 	    // Marker Clicked EE
 	    $scope.$on('MARKER_CLICKED', function(event, id) {
+	      console.log(id);
 	      $scope.showDetail(id);
 	    });
 
 
 	    // Show marker detail by id
 	    $scope.showDetail = function(pinId) {
-	      // $scope.comments = [{
-	      //   content : 'Sam'
-	      // }];
-	      $scope.getComments(pinId);
+	      console.log(pinId);
 	      $scope.activePin = {};
 	      $scope.activePin = $scope.allPins.filter(function(pin) {
 	        return pin._id === pinId; // Filter out the appropriate one
@@ -239,24 +235,6 @@
 	      $scope.actions.detail = true;
 	      $scope.stopPinning(false);
 	    };
-
-	    // Get all comments for a post
-	    $scope.getComments = function(pinId) {
-	      $scope.comments = [];
-	      Comment.getComments(pinId).then(function(res) {
-	        $scope.comments = res.data
-	      });
-	    };
-
-	    // Post new comment
-	    $scope.postComment = function(newComment, pinId) {
-	      if (newComment.content.length > 7) {
-	        Comment.postComment(newComment, pinId).then(function(res) {
-	          if ($scope.comments)
-	            $scope.comments.push(res.data);
-	        });
-	      }
-	    }
 
 
 	    // Get all users marker and place on map
@@ -48709,27 +48687,54 @@
 /***/ function(module, exports) {
 
 	module.exports = function(app) {
-		app.directive('pinDetail', function() {
-			return {
-				restrict: 'AEC',
-				replace: true,
-				templateUrl: 'templates/detail.html',
-				scope: {
-					activePin: '=',
-					show: '='
-				},
-				link: function(scope, elems, attr){
-					scope.$watch(function() {
-						return scope.activePin;
-					}, function() {
-						console.log(scope.activePin);
-					});
-				},
-				controller: function($scope) {
-					console.log($scope.show);
-				}
-			}
-		});
+	  app.directive('pinDetail', function() {
+	    return {
+	      restrict: 'AEC',
+	      replace: true,
+	      templateUrl: 'templates/detail.html',
+	      scope: {
+	        activePin: '=',
+	        show: '='
+	      },
+	      link: function($scope, elems, attr) {
+	        $scope.$watch(function() {
+	          return $scope.activePin
+	        }, function() {
+	        	$scope.getComments($scope.activePin._id);
+	        })
+
+	      },
+	      controller: function($scope, Comment) {
+
+	        // Post new comment
+	        $scope.postComment = function(newComment, pinId) {
+	          if (newComment.content.length > 7) {
+	            Comment.postComment(newComment, pinId).then(function(res) {
+	              if ($scope.activePin.comments)
+	                $scope.activePin.comments.push(res.data);
+	            });
+	          }
+	        }
+
+	        // Get all comments for a post
+	        $scope.getComments = function(pinId) {
+	          $scope.activePin.comments = [];
+	          Comment.getComments(pinId).then(function(res) {
+	            $scope.activePin.comments = res.data
+	          });
+	        };
+
+	        // Remove comment from DB
+	        $scope.removeComment = function(comment) {
+	          $scope.
+	          Comment.removeComment(comment._id).then(function(res) {
+
+	          })
+	        }
+
+	      }
+	    }
+	  });
 	}
 
 /***/ }
